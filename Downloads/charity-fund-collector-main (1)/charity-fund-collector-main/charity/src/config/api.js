230a546ed13cebba2fdcb449f47b3011a1fc8256
Monhit Raju <monhitrajul@gmail.com>
2025-12-18 11:36:23 +0530

    Fix API URL connection

--- Downloads/charity-fund-collector-main (1)/charity-fund-collector-main/charity/src/config/api.js
+++ Downloads/charity-fund-collector-main (1)/charity-fund-collector-main/charity/src/config/api.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000';
+const API_BASE_URL = 'https://charity-fund-collector.onrender.com';
 
 export default API_BASE_URL;
